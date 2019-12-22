498b41180b8c210a6df4c2ee7dcae4861beb4ace
uriyahav <uriyahavshush@gmail.com>
2019-12-22 10:51:22 +0200

    finished datastructure start algo

--- src/dataStructure/nodeData.java
+++ src/dataStructure/nodeData.java
@@ -10,31 +10,31 @@ public class nodeData implements node_data {
 	Point3D point;
 	Color tag;
 	String info;
-	
+
 	public nodeData() {
-	this.point= Point3D.ORIGIN;	
+		this.point= Point3D.ORIGIN;	
 		this.key= 0;
-		this.setTag(0);
+		this.tag= Color.black;
 		this.weight = 0;
 	}
-	public nodeData(int key, int weight, Point3D point, Color tag) {
+	public nodeData(int key, int weight, Point3D point) {
 		this.key=key;
 		this.weight=weight;
 		this.point=point;
-		this.tag=tag;
+		this.tag = Color.black;
 	}
-	
+
 	public nodeData(nodeData a) {
 		this.key = a.key;
 		this.weight = a.weight;
 		this.point=a.point;
 		this.tag=a.tag;
 	}
-		
-		
-		@Override
+
+
+	@Override
 	public int getKey() {
-			return this.key;
+		return this.key;
 	}
 
 	@Override
@@ -59,23 +59,23 @@ public class nodeData implements node_data {
 
 	@Override
 	public String getInfo() {
-		//return this.info;
-		return ("key = " + this.key+ "weight= " +this.weight + "Point3D= " + this.point+"tag = "+ this.tag); 
+		String s ="";
+		return s;
 	}
 
 	@Override
 	public void setInfo(String s) {
-		// TODO Auto-generated method stub
+		this.info=s;
 	}
 
 	@Override
 	public int getTag() {
-			return this.tag.getRGB();
+		return this.tag.getRGB();
 	}
 
 	@Override
 	public void setTag(int t) {
-				this.setTag(t);
+		this.tag= new Color(t);
 	}
 
 }
